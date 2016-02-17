a8eeb97906169a9a52caa053d9a4a3b86a208fc9
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-02-17 20:05:47 -0300

    Ventana lista

--- main.py
+++ main.py
@@ -4,5 +4,24 @@ import sys
 import pygame
 from pygame.locals import *
 
+ANCHO = 500
+ALTO = 600
+FONDO = (0, 0, 0) # NEGRO
 
-pygame.init()
+def main():
+    pygame.init()
+
+    # Creacion Ventana
+    ventana = pygame.display.set_mode((ANCHO, ALTO))
+    # Titulo de ventana
+    pygame.display.set_caption("SpaceShip")
+
+    while True:
+        ventana.fill(FONDO)
+        for evento in pygame.event.get():
+            if evento.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+main()
