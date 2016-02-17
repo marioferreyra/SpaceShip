46021c82c21c4a413557a74a4c1d1202c879d476
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-02-17 20:38:19 -0300

    Nueva imagen de asteroide y fondo. Maquetado de como seria el juego

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import sys
 import pygame
 from pygame.locals import *
 
-ANCHO = 500
-ALTO = 600
-FONDO = (0, 0, 0) # NEGRO
+ANCHO = 600
+ALTO = 680
+FONDO = (255, 255, 255) # BLANCO
+
 
 def main():
     pygame.init()
@@ -16,12 +17,22 @@ def main():
     # Titulo de ventana
     pygame.display.set_caption("SpaceShip")
 
+    nave = pygame.image.load("images/nave.png")
+    asteroide1 = pygame.image.load("images/asteroide1.png")
+    asteroide2 = pygame.image.load("images/asteroide2.png")
+    asteroide3 = pygame.image.load("images/asteroide3.png")
+    
     while True:
         ventana.fill(FONDO)
+        ventana.blit(nave, (225, 470))
+        ventana.blit(asteroide1, (0, 0))
+        ventana.blit(asteroide2, (225, 0))
+        ventana.blit(asteroide3, (470, 0))
         for evento in pygame.event.get():
             if evento.type == QUIT:
                 pygame.quit()
                 sys.exit()
+        pygame.display.update()
 
 
 main()
