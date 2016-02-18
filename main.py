d478c971bf54d27e7d6258cb1ea7656acd28e2fa
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-02-18 00:23:47 -0300

    Inicio de clase para la Nave, falta terminar. Fondo de estrellas implementado

--- main.py
+++ main.py
@@ -6,7 +6,28 @@ from pygame.locals import *
 
 ANCHO = 600
 ALTO = 680
-FONDO = (255, 255, 255) # BLANCO
+
+
+# class Nave(pygame.sprite.Sprite):
+
+#     def __init__(self, posX, posY):
+#         pygame.sprite.Sprite.__init__(self)
+
+#         self.posX = posX
+#         self.posY = posY
+#         self.imagen = pygame.image.load("image/nave.png")
+
+#         self.rectangulo = self.imagen.get_rect()
+
+#     def movimientoDerecha(self):
+#         pass
+
+#     def movimientoIzquierda(self):
+#         pass
+
+#     def dibujar(self, superficie):
+#         superficie.blit()
+
 
 
 def main():
@@ -17,14 +38,15 @@ def main():
     # Titulo de ventana
     pygame.display.set_caption("SpaceShip")
 
+    fondo = pygame.image.load("images/fondo.jpg")
     nave = pygame.image.load("images/nave.png")
     asteroide1 = pygame.image.load("images/asteroide1.png")
     asteroide2 = pygame.image.load("images/asteroide2.png")
     asteroide3 = pygame.image.load("images/asteroide3.png")
     
     while True:
-        ventana.fill(FONDO)
-        ventana.blit(nave, (225, 470))
+        ventana.blit(fondo, (0, 0))
+        ventana.blit(nave, (225, 550))
         ventana.blit(asteroide1, (0, 0))
         ventana.blit(asteroide2, (225, 0))
         ventana.blit(asteroide3, (470, 0))
