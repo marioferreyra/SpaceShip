e6d842e1650971a31d73d6574fc79d0422779067
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-02-20 19:51:08 -0300

    Clase de la Nave implementada y funcionando basicamente

--- main.py
+++ main.py
@@ -7,25 +7,27 @@ ANCHO = 600
 ALTO = 680
 
 
-# class Nave(pygame.sprite.Sprite):
+class Nave(pygame.sprite.Sprite):
 
-#     def __init__(self, posX, posY):
-#         pygame.sprite.Sprite.__init__(self)
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
 
-#         self.posX = posX
-#         self.posY = posY
-#         self.imagen = pygame.image.load("image/nave.png")
+        self.imagen = pygame.image.load("images/nave.png")
+        self.rect = self.imagen.get_rect()
+        
+        self.rect.centerx = ANCHO/2
+        self.rect.centery = 610
+        print self.rect
+    
+    def dibujar(self, superficie):
+        superficie.blit(self.imagen, self.rect)
 
-#         self.rectangulo = self.imagen.get_rect()
+    def moverDerecha(self):
+        self.rect.left += 100
 
-#     def movimientoDerecha(self):
-#         pass
+    def moverIzquierda(self):
+        self.rect.left -= 100
 
-#     def movimientoIzquierda(self):
-#         pass
-
-#     def dibujar(self, superficie):
-#         superficie.blit()
 
 
 
@@ -38,7 +40,7 @@ def main():
     pygame.display.set_caption("SpaceShip")
 
     fondo = pygame.image.load("images/fondo.jpg")
-    nave = pygame.image.load("images/nave.png")
+    nave = Nave()
     asteroide1 = pygame.image.load("images/asteroide1.png")
     asteroide2 = pygame.image.load("images/asteroide2.png")
     asteroide3 = pygame.image.load("images/asteroide3.png")
@@ -49,29 +51,19 @@ def main():
 
     while True:
         ventana.blit(fondo, (0, 0))
-        ventana.blit(nave, (posX, posY))
         ventana.blit(asteroide1, (0, 0))
         ventana.blit(asteroide2, (225, 0))
         ventana.blit(asteroide3, (470, 0))
+        nave.dibujar(ventana)
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_LEFT:
-                    if posX == 470:
-                        posX = 225
-                    elif posX == 225:
-                        posX = 0
+                    nave.moverIzquierda()
                 elif evento.key == pygame.K_RIGHT:
-                    if posX == 0:
-                        posX = 225
-                    elif posX == 225:
-                        posX = 470
-                elif evento.key ==  pygame.K_UP:
-                    posY -= 30
-                elif evento.key ==  pygame.K_DOWN:
-                    posY += 30
+                    nave.moverDerecha()
 
         reloj.tick(20) # 20 FPS
         pygame.display.update()
