d1c3e9e8721211b61b4abdf40beae0dba558e542
Mario E. Ferreyra <ferreyramario7@gmail.com>
2016-02-21 19:35:16 -0300

    Actualizado

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class Nave(pygame.sprite.Sprite):
         
         self.rect.centerx = ANCHO/2
         self.rect.centery = 610
-        print self.rect
     
     def dibujar(self, superficie):
         superficie.blit(self.imagen, self.rect)
@@ -55,8 +54,11 @@ class Asteroide(pygame.sprite.Sprite):
     def dibujar(self, superficie):
         superficie.blit(self.imagen, self.rect)
 
-    def movimieto(self):
+    def mover(self):
         self.rect.top += 10
+
+    def detener(self):
+        self.rect.top = 0
 
 
 
@@ -68,14 +70,15 @@ def main():
     ventana = pygame.display.set_mode((ANCHO, ALTO))
     # Titulo de ventana
     pygame.display.set_caption("SpaceShip")
-
     fondo = pygame.image.load("images/fondo.jpg")
+    fps = pygame.time.Clock()
+
+    en_juego = True
+
     nave = Nave()
     asteroide1 = Asteroide("images/asteroide1.png", 22, 10)
     asteroide2 = Asteroide("images/asteroide2.png", 236, 10)
     asteroide3 = Asteroide("images/asteroide3.png", 450, 10)
-
-    reloj = pygame.time.Clock()
 
     while True:
         ventana.blit(fondo, (0, 0))
@@ -84,36 +87,45 @@ def main():
         asteroide2.dibujar(ventana)
         asteroide3.dibujar(ventana)
         
-        aleatorio = random.randint(1, 3)
-        if aleatorio == 1:
-            asteroide1.movimieto()
-        elif aleatorio == 2:
-            asteroide2.movimieto()
-        elif aleatorio == 3:
-            asteroide3.movimieto()
+        if en_juego:
+            aleatorio = random.randint(1, 3)
+            if aleatorio == 1:
+                asteroide1.mover()
+            elif aleatorio == 2:
+                asteroide2.mover()
+            elif aleatorio == 3:
+                asteroide3.mover()
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_LEFT:
-                    nave.moverIzquierda()
-                elif evento.key == pygame.K_RIGHT:
-                    nave.moverDerecha()
-                elif evento.key == pygame.K_UP:
-                    nave.moverArriba()
-                elif evento.key == pygame.K_DOWN:
-                    nave.moverAbajo()
+
+            if en_juego:
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_LEFT:
+                        nave.moverIzquierda()
+                    elif evento.key == pygame.K_RIGHT:
+                        nave.moverDerecha()
+                    elif evento.key == pygame.K_UP:
+                        nave.moverArriba()
+                    elif evento.key == pygame.K_DOWN:
+                        nave.moverAbajo()
 
         if nave.rect.colliderect(asteroide1.rect):
             nave.destruccion()
+            asteroide1.detener()
+            en_juego = False
         if nave.rect.colliderect(asteroide2.rect):
             nave.destruccion()
+            asteroide2.detener()
+            en_juego = False
         if nave.rect.colliderect(asteroide3.rect):
             nave.destruccion()
+            asteroide3.detener()
+            en_juego = False
 
-        reloj.tick(20) # 20 FPS
+        fps.tick(20) # 20 FPS
         pygame.display.update()
 
 
